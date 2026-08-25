7cc1527ece799b0811fd2e0484608969238cad3a
pradeep bishnoi <pradeepbishnoi9601@gmail.com>
2026-08-25 13:03:46 +0530

    update app and model test files ci-cd 7

--- tests/test_model.py
+++ tests/test_model.py
@@ -19,9 +19,9 @@ class TestModelLoading(unittest.TestCase):
 
         # for the production use
          # Set up DagsHub credentials for MLflow tracking
-        dagshub_token = os.getenv("CAPSTONE_TEST")
+        dagshub_token = os.getenv("DAGSHUB_TOKEN")
         if not dagshub_token:
-            raise EnvironmentError("CAPSTONE_TEST environment variable is not set")
+            raise EnvironmentError("DAGSHUB_TOKEN environment variable is not set")
 
         os.environ["MLFLOW_TRACKING_USERNAME"] = dagshub_token
         os.environ["MLFLOW_TRACKING_PASSWORD"] = dagshub_token
